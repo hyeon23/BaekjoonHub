3586a6d9b75965f3f9b18b0adb7735c4d04e291a
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-05-11 19:47:32 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -5,33 +5,31 @@ using namespace std;
 
 int main()
 {
-    int *arr;
+        vector<int> vec;
     int N, M;
     cin >> N >> M;
-
-    arr= new int[N];
 
     int i, j, k;
 
     for (int jj = 0; jj < N; ++jj)
     {
-        arr[jj] = jj + 1;
+        vec.push_back(jj + 1);
     }
 
     for (int a = 0; a < M; ++a)
     {
         cin >> i >> j;
-        int temp = arr[i-1];
-        arr[i-1] = arr[j-1];
-        arr[j-1] = temp;
+        int temp = vec[i-1];
+        vec[i-1] = vec[j-1];
+        vec[j-1] = temp;
     }
 
     for (int kk = 0; kk < N - 1; ++kk)
     {
-        cout << arr[kk] << " ";
+        cout << vec[kk] << " ";
     }
 
-    cout << arr[N - 1];
+    cout << vec[N - 1];
 
     return 0;
 }
