826ae777953174e870d2dd352b1a6f150097ea95
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-05-11 20:01:38 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
@@ -4,29 +4,16 @@
 using namespace std;
 
 int main(){
-    int arr[31]{0};
-    int isSubmitted;
-    
-    int arr2[2]{0};
-
-
+//sol2
+    int a;
+    bool check[31] = {0,};
     for(int i = 1; i <= 28; ++i){
-        cin >> isSubmitted;
-
-        arr[isSubmitted] = 1;
+        cin >> a;
+        check[a] = 1;
     }
-
-    int count = 0;
-
-    for(int i = 1; i <= 30; ++i){
-
-        if(arr[i] == 0){
-            arr2[count++] = i;
-        }
+    for(int j = 1; j <= 30; ++j){
+        if(check[j] == 0)
+            cout << j << "\n";
     }
-
-    cout << min(arr2[0], arr2[1]) << endl;
-    cout << max(arr2[0], arr2[1]) << endl;
-
     return 0;
 }
