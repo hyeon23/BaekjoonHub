61fc8522f2db4cbc839b2e4e747bc941a7cd7188
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-05-11 19:52:00 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -5,31 +5,20 @@ using namespace std;
 
 int main()
 {
-        vector<int> vec;
-    int N, M;
-    cin >> N >> M;
-
-    int i, j, k;
-
-    for (int jj = 0; jj < N; ++jj)
-    {
-        vec.push_back(jj + 1);
+    int n, m, a, b;
+    cin >> n >> m;
+    int* bas = new int[n+1];
+    for(int i = 1; i <= n; ++i){
+        bas[i] = i;
     }
-
-    for (int a = 0; a < M; ++a)
-    {
-        cin >> i >> j;
-        int temp = vec[i-1];
-        vec[i-1] = vec[j-1];
-        vec[j-1] = temp;
+            
+    while(m--){
+        cin >> a >> b;
+        swap(bas[a], bas[b]);
     }
-
-    for (int kk = 0; kk < N - 1; ++kk)
-    {
-        cout << vec[kk] << " ";
+    for(int i = 1; i <= n; ++i){
+        cout << bas[i] << ' ';
     }
-
-    cout << vec[N - 1];
 
     return 0;
 }
