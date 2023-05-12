96ef61be85519cbf93f6e19673c7c80e24bc7cf9
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-05-12 12:39:11 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    int *count;
+    string *s;
+
+    cin >> N;
+
+    count = new int[N];
+    s = new string[N];
+
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> count[i] >> s[i];
+    }
+
+    for (int i = 0; i < N; ++i)
+    {
+        for (int k = 0; k < s[i].length(); ++k)
+        {
+            for (int j = 0; j < count[i]; ++j)
+            {
+                cout << s[i][k];
+            }
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
