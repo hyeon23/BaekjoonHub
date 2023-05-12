ae2dfea5d30c3faa9d1e7fddd04b841be6748f9e
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-05-12 11:21:09 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
@@ -6,10 +6,24 @@ using namespace std;
 
 int main()
 {
-    string s;
-    string alphabet = "abcdefghijklmnopqrstuvwxyz";
-    cin >> s;
-    for (int i = 0; i < alphabet.length(); i++)
-        cout << (int)s.find(alphabet[i]) << " ";
+    char S[101];
+    int alpha[26];
+    cin >> S;
+
+    for (int i = 0; i < 26; ++i)
+    {
+        alpha[i] = -1;
+    }
+
+    for (int i = 0; i < strlen(S); ++i)
+    {
+        if (alpha[(int)S[i] - 97] < 0)
+            alpha[(int)S[i] - 97] = i;
+    }
+
+    for (int i = 0; i < 26; ++i)
+    {
+        cout << alpha[i] << " ";
+    }
     return 0;
 }
