5fbd28e3592505b8486db9bc37cae313b3f7c630
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-05-21 01:31:03 +0900

    [Silver V] Title: 분수찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.cc
+++ 백준/Silver/1193. 분수찾기/분수찾기.cc
@@ -5,22 +5,17 @@ int main(){
     int X;
 
     cin >> X;
-    int count = 1;
-    while(true){
-        if(X - count > 0){
-            X -= count;
-            count++;
-        }
-        else{
-            break;
-        }
+    int i = 1;
+    while(X > i){
+        X -= i;
+        i++;
     }
 
-    if(count % 2 == 0){//짝
-        cout << X << "/" << count - X + 1;
+    if(i % 2 == 0){//짝
+        cout << X << "/" << i - X + 1;
     }
     else{//홀
-        cout << count - X + 1 << "/" << X;
+        cout << i - X + 1 << "/" << X;
     }
 
     return 0;
