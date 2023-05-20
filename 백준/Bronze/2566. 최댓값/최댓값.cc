8f653cb359b14e299a83db0f0b0229eb6f5d1d69
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-05-20 15:48:42 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[9][9] = {};
+
+    int maxVal = -1;
+    int maxRow = -1;
+    int maxCol = -1;
+
+    for(size_t i = 0; i < 9; ++i){
+        for (size_t j = 0; j < 9; ++j)
+        {
+            int tempval;
+            cin >> tempval;
+
+            arr[i][j] = tempval;
+
+            if(arr[i][j] > maxVal){
+                maxVal = arr[i][j];
+                maxRow = i + 1;
+                maxCol = j + 1;
+            }
+        }
+    }
+
+    cout << maxVal << "\n";
+    cout << maxRow << " " << maxCol;
+
+    return 0;
+}
