bc146ac201a042614ffefc261455a31caf454bbb
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-06-08 15:36:18 +0900

    [Silver V] Title: 숫자 카드, Time: 468 ms, Memory: 25256 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.cc
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -1,27 +1,28 @@
-#include <vector>
+#include <set>
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
 int main(){
-    int N, M;
-    cin >> N;
-    vector<int> vec1(N);
-    for(int i = 0; i < N; ++i)
-        cin >> vec1[i];
-    cin >> M;
-    vector<pair<int, int>> vec2;
-    for(int i = 0; i < M; ++i){
-        int x;
-        cin >> x;
-        vec2.push_back({x, 0});
-    }
-    sort(vec1.begin(), vec1.end());
-    for(int i = 0; i < vec2.size(); ++i){
-        if(binary_search(vec1.begin(), vec1.end(), vec2[i].first))
-            cout << 1 << " ";
-        else
-            cout << 0 << " ";
-    }
-    return 0;
+    ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	set<int> card;
+	int N; 
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		int num;
+		cin >> num;
+		card.insert(num);
+	}
+	int M; 
+	cin >> M;
+	for (int i = 0; i < M; i++)
+	{
+		int num;
+		cin >> num;
+		if (card.find(num) != card.end()) cout << "1 ";
+		else cout << "0 ";
+	}
+	return 0;
 }
