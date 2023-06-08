a06a6f757b7761f533094cd6a73bd26c0bd3747a
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-06-08 22:36:40 +0900

    [Silver IV] Title: 듣보잡, Time: 124 ms, Memory: 12996 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -0,0 +1,32 @@
+#include <set>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    int N, M;
+    string temp;
+    cin >> N >> M;
+    set<string> set1;
+    set<string> set2;
+    vector<string> vec;
+    for(int i = 0; i < N; ++i){
+        cin >> temp;
+        set1.insert(temp);
+    }
+    for(int i = 0; i < M; ++i){
+        cin >> temp;
+        set2.insert(temp);
+    }
+    for(auto i : set1){
+        if(set2.find(i) != set2.end()){
+            vec.push_back(i);
+        }
+    }
+    sort(vec.begin(), vec.end());
+    cout << vec.size() << '\n';
+    for(auto i : vec){
+        cout << i << '\n';
+    }
+}
