e2710cd0f819553e55f898143e2f5f3a690786c2
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-06-09 08:42:30 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int A, B;
+
+    while(cin >> A >> B)
+        cout << A + B << '\n';
+}
