225adcdff808dad6d7b19ee2c90a4ee9e6cdf2d6
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-06-08 19:07:28 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 72 ms, Memory: 9948 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
@@ -3,23 +3,24 @@
 #include <string>
 #include <map>
 #include <set>
-
 using namespace std;
-
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     int N;
-    string s1, s2;
     cin >> N;
-    multimap<string, string> map;
+    set<string> set;
     for(int i = 0; i < N; ++i){
+        string s1, s2;
         cin >> s1 >> s2;
+
         if(s2 == "enter")
-            map.insert({s1, s2});
-        else if(s2 == "leave")
-            map.erase(s1);
+            set.insert(s1);
+        else
+            set.erase(s1);
     }
-    for(auto iter = map.rbegin(); iter != map.rend(); ++iter)
-        cout << (*iter).first << '\n';
+
+    for(auto iter = set.rbegin(); iter != set.rend(); ++iter){
+        cout << *iter << "\n";
+    }
 }
