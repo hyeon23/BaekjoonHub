7a1e5130e5ee116147d0e6b7410506f1c1546931
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-06-08 21:59:38 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 320 ms, Memory: 3852 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,19 +1,21 @@
 #include <iostream>
+#include <algorithm>
 #include <unordered_map>
 using namespace std;
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    unordered_map<int, int> m;
-    int N, M, card;      
+    int arr[500002];
+    int N, M, card;
     cin >> N;
     for(int i = 0; i < N; ++i){
         cin >> card;
-        m[card]++;
-    }  
+        arr[i] = card;
+    }
+    sort(arr, arr + N);
     cin >> M;
     for(int i = 0; i < M; ++i){
         cin >> card;
-        cout << m[card] << " ";
+        cout << upper_bound(arr, arr+N, card) - lower_bound(arr, arr+N, card) << " ";
     }
 }
