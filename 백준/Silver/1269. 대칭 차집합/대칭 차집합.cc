ee19c5dde7b6c3ebdbdd412d570d2e8a751fbacc
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-06-08 23:36:18 +0900

    [Silver IV] Title: 대칭 차집합, Time: 152 ms, Memory: 11396 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -3,21 +3,20 @@
 #include <algorithm>
 #include <iostream>
 using namespace std;
-//sol1: set으로 줄이고 줄여도 안됨 ㅈㄹ한다.
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    int N, M, temp, count = 0;
+    int N, M, temp, count;
     cin >> N >> M;
-    set<int> set1;
+    map<int, int> map;
     for(int i = 0; i < N; ++i){
         cin >> temp;
-        set1.insert(temp);
+        map.insert({temp, 0});
     }
     for(int i = 0; i < M; ++i){
         cin >> temp;
-        if(set1.find(temp) != set1.end()){
+        if(map.find(temp) != map.end()){
             ++count;
         }
     }
