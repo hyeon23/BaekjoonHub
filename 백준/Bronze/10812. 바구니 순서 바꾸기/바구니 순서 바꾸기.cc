69a1dd4595dec13578d0542a59e14e8df9e92810
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-06-09 13:28:23 +0900

    [Bronze II] Title: 바구니 순서 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10812. 바구니 순서 바꾸기/바구니 순서 바꾸기.cc
+++ 백준/Bronze/10812. 바구니 순서 바꾸기/바구니 순서 바꾸기.cc
@@ -1,41 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-    int N, M, i, j, k;
+    //sol2: #include <algorithm> rotate()를 이용한 간단 구현
+    int n, m; cin >> n >> m; vector<int> v;
 
-    cin >> N >> M;
-
-    vector<int> buskets;
-    vector<int> busketsTEMP;
-
-    for (int a = 0; a < N + 1; ++a)
-    {
-        buskets.push_back(a);
-        busketsTEMP.push_back(-1);
+    for(int i = 1; i <= n; ++i){
+        v.push_back(i);
     }
 
-    for (int a = 0; a < M; ++a)
-    {
-        cin >> i >> j >> k;
+    while(m--){
+        int i, j, k; cin >> i >> j >> k;
 
-        for (int b = 1; b < N + 1; ++b)
-        {
-            busketsTEMP[b] = buskets[b];
-        }
-
-        for (int c = i; c <= j; ++c)
-        {
-            buskets[c] = busketsTEMP[i + (k + c - 2 * i) % (j - i + 1)];
-        }
+        //범위 (i - 1) ~ (j - 1)
+        //시작 값은 k번째 값으로 변경 & rotation
+        //왼쪽으로
+        rotate(v.begin() + (i-1), v.begin() + (k-1), v.begin() + j);
     }
 
-    for(int d = 1; d < N + 1; ++d){
-        cout << buskets[d] << " ";
-    }
+    for(int a : v)
+        cout << a << " ";
+
+    cout << "\n";
 
     return 0;
 }
