adb905b8bdff37eed3af6f1f6dac28769d9416d4
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-06-15 18:55:11 +0900

    [Silver IV] Title: 다음 소수, Time: 264 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/4134. 다음 소수/다음 소수.cc
+++ 백준/Silver/4134. 다음 소수/다음 소수.cc
@@ -1,43 +1,44 @@
 #include <iostream>
-#include <algorithm>
-#include <cstring>
 #include <cmath>
-
 using namespace std;
 
-bool isPrimeNum(long long n)
+bool is_prime(long long x)
 {
-for (int i = 2; i <= sqrt(n); i++) {
-if (n % i == 0) {
-return false;
-}
-}
-
-return true;
+    for (long long i = 2; i <= sqrt(x); ++i)
+    {
+        if (x % i == 0)
+            return false;
+    }
+    return true;
 }
 
 int main()
 {
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
-cout.tie(NULL);
+    // 소수판단: 그 값의 약수들의 곲은 그 값의 제곱근을 기준으로 대칭
+    //-제곱근 이하의 값 까지만 검사를 하면 나머지는 검사를 할 필요가 없다.
+    //-sqrt(int x): 제곱근 구하는 함수
 
-int T;
-cin >> T;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-long long n;
-for (int i = 0; i < T; i++) {
-cin >> n;
-if (n >= 0 && n <= 2)
-cout << 2 << endl;
-else if (n == 3)
-cout << 3 << endl;
-else {
-while (!isPrimeNum(n))
-n++;
-cout << n << endl;
-}
-}
+    unsigned int T, x;
 
-return 0;
+    cin >> T;
+
+    for (unsigned int i = 0; i < T; ++i)
+    {
+        cin >> x;
+
+        if (x >= 0 && x <= 2)
+            cout << 2 << "\n";
+        else if (x == 3)
+            cout << 3 << "\n";
+        else
+        {
+            while (!is_prime(x))
+                ++x;
+            cout << x << "\n";
+        }
+    }
 }
