8908cfa0412d1dce33f49341e26a45b1c7239f95
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-06-15 13:13:40 +0900

    [Bronze I] Title: 최소공배수, Time: 40 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int divide(int x, int y){
+    if(x % y == 0) return y;
+    else return divide(y, x % y);
+}
+
+int main(){
+    int T, A, B;
+    cin >> T;
+    for(int i = 0; i < T; ++i){
+        cin >> A >> B;
+        if(A >= B)
+            cout << A * B / divide(A, B) << "\n";
+        else
+            cout << A * B / divide(B, A) << "\n";
+    }
+}
