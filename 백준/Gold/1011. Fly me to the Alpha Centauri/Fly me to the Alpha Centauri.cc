4fc492ee9e986c4b284ce8f198b79f6e883500ad
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-07-02 13:16:18 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.cc
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.cc
@@ -1,24 +1,23 @@
 #include <cmath>
 #include <iostream>
-
 using namespace std;
 
 int main(){
-    int T;
-    cin >> T;
-    
-    while(T--){
-        long long int x, y, move, max = 0;
-        cin >> x >> y;
-        while(max * max <= y - x)
-            max++;
-        max--;
-        move = 2 * max - 1;
-        long long rem = y - x - max * max;
-        rem = (long long)ceil((double)rem / (double)max);
-        move += rem;
-        cout << move << '\n';
-    }
-
-    return 0;
+	cin.tie(0);
+	int t;
+	cin >> t;
+	
+	long long x, y;
+	
+	for(int i=0; i<t; i++){
+		cin >> x >> y;
+		double dist = y-x; // 두 지점 사이의 거리
+		double dpow = sqrt(dist); // 거리의 제곱근
+		int pow = round(sqrt(dist)); // 거리의 제곱근을 반올림
+		
+		if(dpow <= pow) cout << pow * 2 - 1 << "\n"; 
+		else cout << pow * 2 << "\n"; 
+	}
+		
+	return 0;
 }
