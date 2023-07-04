66d0f10ef0c544e75f852b4b797fbf47f13e1287
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-07-05 00:34:37 +0900

    [unrated] Title: n의 배수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181937. n의 배수/n의 배수.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num, int n) {
+    int answer = 0;
+    if(num % n == 0)
+        return 1;
+    else
+        return 0;
+}
