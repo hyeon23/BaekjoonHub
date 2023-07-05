d72e506d586bad9544008a8af6c9de4c8a9f040e
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-07-05 14:08:04 +0900

    [unrated] Title: 수열과 구간 쿼리 4, Time: 0.36 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    vector<int> answer;
+    for(int i = 0; i < queries.size(); ++i){
+        for(int j = queries[i][0]; j <= queries[i][1]; ++j){
+            if(j % queries[i][2] == 0)
+                arr[j]++;
+        }
+    }
+    return arr;
+}
