210472b32ea4394d01e2407544458e99898a2f58
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-07-06 15:32:21 +0900

    [unrated] Title: 순서 바꾸기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.cpp
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.cpp
@@ -4,11 +4,7 @@
 using namespace std;
 
 vector<int> solution(vector<int> num_list, int n) {
-    vector<int> answer;
-    for(auto it = num_list.begin() + n; it != num_list.end(); it++)
-        answer.push_back(*it);
-    for(auto it = num_list.begin(); it != num_list.begin() + n; it++)
-        answer.push_back(*it);
-    
+    vector<int> answer(num_list.begin(), num_list.begin() + n);
+    answer.insert(answer.begin(), num_list.begin() + n, num_list.end());
     return answer;
 }
