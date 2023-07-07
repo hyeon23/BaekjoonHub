6d9091d6c50d08cff449bd77eaeee6d87d915c69
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-07-07 20:42:05 +0900

    [unrated] Title: 문자열 묶기, Time: 6.27 ms, Memory: 15.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.cpp
@@ -0,0 +1,18 @@
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<string> strArr) {
+    int curmax = -1;;
+    map<int, int> map;
+    for(int i = 0; i < strArr.size(); ++i)
+        map[strArr[i].size()]++;
+    
+    for(auto it = map.begin(); it != map.end(); ++it)
+        if((*it).second > curmax){
+            curmax = (*it).second;
+        }
+    return curmax;
+}
