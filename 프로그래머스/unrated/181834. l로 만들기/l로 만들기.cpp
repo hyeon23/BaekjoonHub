5c00ffd54ab24ebeb215d27f3f4a10cc03690c5f
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-07-09 22:49:10 +0900

    [unrated] Title: l로 만들기, Time: 0.36 ms, Memory: 3.93 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    for(int i = 0; i < myString.size(); ++i)
+        if(myString[i] < 'l')
+            myString[i] = 'l';
+    return myString;
+}
