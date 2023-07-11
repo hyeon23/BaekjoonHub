1562ab84a7040acec8a497204996b380e69a920a
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-07-11 21:52:56 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for(int i = 1; i <= n; ){
+        if(answer % 3 == 0 || to_string(answer).find('3') != string::npos){
+            ++answer;
+        }
+        else{
+            ++answer;
+            ++i;
+        }
+    }
+    return answer - 1;
+}
