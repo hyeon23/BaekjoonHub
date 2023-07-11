14e64e62d37d67d0790ed1b4b93351fb30e05a8c
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-07-11 19:52:01 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int answer = 0;
+    while(b % 2 == 0)
+        b /= 2;
+    while(b % 5 == 0)
+        b /= 5;
+    return (a % b == 0)?1:2;
+}
