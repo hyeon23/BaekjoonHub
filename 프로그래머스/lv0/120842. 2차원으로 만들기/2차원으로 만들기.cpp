ce07ff25eb7a72036f294d8064d2f73ffc3b4e20
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-07-12 20:24:50 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.cpp
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.cpp
@@ -7,18 +7,10 @@ vector<vector<int>> solution(vector<int> num_list, int n) {
     vector<vector<int>> answer(num_list.size() / n, vector<int>(n, 0));
     //n X num_list.size() / n의 배열로 별환
     int cnt = 0;
-    for(int j = 0; j < num_list.size() / n; ++j){
+    for(int j = 0; j < num_list.size() / n; ++j)
         for(int i = 0; i < n; ++i){
-            answer[j][i] = num_list[cnt];
-            cnt++;
+            answer[j][i] = num_list[cnt++];
         }
             
-    }
-    
-    
-            
-    
-    
-        
     return answer;
 }
