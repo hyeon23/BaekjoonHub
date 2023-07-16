d62809077c4d227d53ffd410e32380c6c4c9d86a
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-07-16 16:35:51 +0900

    [level 1] Title: 내적, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/70128. 내적/내적.cpp
@@ -0,0 +1,8 @@
+#include <string>
+#include <vector>
+#include <numeric>
+using namespace std;
+
+int solution(vector<int> a, vector<int> b) {
+    return inner_product(a.begin(), a.end(), b.begin(), 0);
+}
