f76475aaf0d7f39171b65aba634451f2c052bb27
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-07-18 13:50:27 +0900

    [unrated] Title: 카드 뭉치, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.cpp
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.cpp
@@ -1,28 +1,17 @@
 #include <stack>
 #include <string>
 #include <vector>
-#include <iostream>
 #include <algorithm>
 using namespace std;
 
 string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
     for(const auto i : goal){
-        if(i == cards1[0]){
-            cards1.erase(find(cards1.begin(), cards1.end(), i));
-        }
-        else if(i == cards2[0]){
-            cards2.erase(find(cards2.begin(), cards2.end(), i));
-        }
+        if(i == cards1[0])
+            cards1.erase(cards1.begin());
+        else if(i == cards2[0])
+            cards2.erase(cards2.begin());
         else
             return "No";
-        
-        for(const auto i : cards1)
-            cout << i << ' ';
-        cout << '\n';
-        for(const auto i : cards2)
-            cout << i << ' ';
-        cout << '\n';
-        
     }
     return "Yes";
 }
