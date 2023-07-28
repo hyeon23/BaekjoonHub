a4c9eaeb284036bc7423662aff9f0964a0897b2f
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-07-28 12:45:18 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.06 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<long long> fiboarr(2001, -1);
+
+unsigned long long fibonacci(int N){
+    if(fiboarr[N] != -1)
+        return fiboarr[N] % 1234567;
+    
+    fiboarr[N] = (fibonacci(N - 1) % 1234567) + (fibonacci(N - 2) % 1234567);
+    return fiboarr[N] % 1234567;
+}
+
+unsigned long long solution(int n) {
+    unsigned long long answer = 0;
+    //n = 0 -> 0
+    //n = 1 -> 1
+    //n = 2 -> 2
+    //n = 3 -> 3
+    //n = 4 -> 5
+    //n = k -> f(n-1) + f(n-2); => fibonacci(조합 풀이도 가능)
+    //시간 초과 => DP[메모리]를 사용한 피보나치
+    
+    fiboarr[0] = 1;
+    fiboarr[1] = 1;
+    fiboarr[2] = 2;
+    
+    return fibonacci(n);
+}
