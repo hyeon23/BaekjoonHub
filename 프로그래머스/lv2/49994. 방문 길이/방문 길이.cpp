b23d1fbcdf92e9da464bc3d5a6f97383c050d1a5
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-08-01 12:23:53 +0900

    [level 2] Title: 방문 길이, Time: 0.13 ms, Memory: 3.73 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.cpp
@@ -0,0 +1,50 @@
+#include <map>
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int solution(string dirs) {
+    int answer = 0;
+    //start -> to
+    //curpos(x, y) -> secondpos(x, y)
+    //pair<pair<int, int>, pair<int, int>>
+    pair<int, int> curpos = {0, 0};
+    map<pair<pair<int, int>, pair<int, int>>, int> map;
+    
+    for(int i = 0; i < dirs.size(); ++i){
+        if(dirs[i] == 'U'){
+            if(curpos.second + 1 <= 5){
+                pair<int, int> secondpos = {curpos.first, curpos.second + 1};
+                if(map[{curpos, secondpos}] == 0 && map[{secondpos, curpos}] == 0)
+                    map[{curpos, secondpos}] = 1;
+                curpos = secondpos;
+            }
+        }
+        else if(dirs[i] == 'D'){
+            if(curpos.second - 1 >= -5){
+                pair<int, int> secondpos = {curpos.first, curpos.second - 1};
+                if(map[{curpos, secondpos}] == 0 && map[{secondpos, curpos}] == 0)
+                    map[{curpos, secondpos}] = 1;
+                curpos = secondpos;
+            }
+        }
+        else if(dirs[i] == 'R'){
+            if(curpos.first + 1 <= 5){
+                pair<int, int> secondpos = {curpos.first + 1, curpos.second};
+                if(map[{curpos, secondpos}] == 0 && map[{secondpos, curpos}] == 0)
+                    map[{curpos, secondpos}] = 1;
+                curpos = secondpos;
+            }
+        }
+        else if(dirs[i] == 'L'){
+            if(curpos.first - 1 >= -5){
+                pair<int, int> secondpos = {curpos.first - 1, curpos.second};
+                if(map[{curpos, secondpos}] == 0 && map[{secondpos, curpos}] == 0)
+                    map[{curpos, secondpos}] = 1;
+                curpos = secondpos;
+            }
+        }
+    }
+    return map.size() / 2;
+}
