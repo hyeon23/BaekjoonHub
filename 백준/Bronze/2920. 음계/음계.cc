91a2e06fad12861488cb2165bae4ea78124eefcf
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-08-05 12:54:30 +0900

    [Bronze II] Title: 음계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.cc
+++ 백준/Bronze/2920. 음계/음계.cc
@@ -10,12 +10,10 @@ int main(){
         vec.push_back(num);
 
     for(int i = 1; i < vec.size(); ++i){
-        if(vec[i - 1] < vec[i]){
+        if(vec[i - 1] < vec[i])
             asct = true;
-        }
-        else if(vec[i - 1] > vec[i]){
+        else if(vec[i - 1] > vec[i])
             desct = true;
-        }
     }
 
     if(asct && desct) cout << "mixed";
