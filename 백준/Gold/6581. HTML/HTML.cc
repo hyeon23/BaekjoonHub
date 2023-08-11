67f4e551749915047e2059df00806e1bbd29a7ff
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-08-11 14:11:21 +0900

    [Gold V] Title: HTML, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/6581. HTML/HTML.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(0);
+    
+    string str = "";
+    string bar = "--------------------------------------------------------------------------------";
+    int line_len = 0;
+    
+    while(cin >> str) {
+        if(str == "<br>") {
+            cout << "\n";
+            line_len = 0;
+        } else if(str == "<hr>") {
+            if(line_len != 0) cout << "\n";
+            cout << bar << "\n";
+            line_len = 0;
+        } else {
+            if(line_len + str.size() < 80) {
+                if(line_len != 0) {
+                    cout << " ";
+                    line_len++;
+                }
+                cout << str;
+                line_len += str.size();
+            } else {
+                cout << "\n";
+                line_len = 0;
+                cout << str;
+                line_len += str.size();
+            }
+        }
+    }
+}
