43eace0c7174f90625aecb52961bb579d5d62160
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-08-12 13:38:56 +0900

    [Silver I] Title: SMUPC 계산기, Time: 80 ms, Memory: 7332 KB -BaekjoonHub

--- 백준/Silver/21737. SMUPC 계산기/SMUPC 계산기.cc
+++ 백준/Silver/21737. SMUPC 계산기/SMUPC 계산기.cc
@@ -1,45 +1,92 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
 using namespace std;
-using ll = long long;
-ll n, cFlag, output, idx;
-string s;
-vector <ll> nums;
-vector <char> op;
-
-void parsing(){
-    string tmp;
-    for(ll i = 0; i < s.size(); i++){
-        if(s[i] == 'S' || s[i] == 'M' || s[i] == 'U' || s[i] == 'P' || s[i] == 'C' || i == s.size() - 1){
-            if(s[i] == 'C') cFlag = 1;
-            op.push_back(s[i]);
-            if(tmp.size()) nums.push_back(stoi(tmp));
-            tmp = "";
-            continue;
-        }
-        tmp += s[i];
-    }
-}
-
-void operating(){
-    output = nums[0];
-    for(auto o : op){
-        if(o == 'S') output -= nums[++idx];
-        else if(o == 'M') output *= nums[++idx];
-        else if(o == 'U'){
-            if(output < 0) output = -(-output / nums[++idx]);
-            else output = output / nums[++idx];
-        }
-        else if(o == 'P') output += nums[++idx];
-        else cout << output << ' ';
-    }
-}
 
 int main(){
-    cin >> n >> s;
-    
-    parsing();
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int N;
+    cin >> N;//문자의 개수: S: - M: * U: / P: + C: 결과반환
 
-    if(!cFlag){ cout << "NO OUTPUT"; return 0; }
+    string str;
+    cin >> str;
+    queue<int> ans;
 
-    operating();
+    string curnum;
+    char priorcmd;
+    for(int i = 0; i < str.size(); ++i){
+        if(isdigit(str[i])){
+            curnum += str[i];
+        }
+        else if(str[i] == 'S'){//-1
+            if(curnum != "") ans.push(stoi(curnum));
+            ans.push(-1);
+            curnum = "";
+        }
+        else if(str[i] == 'M'){//-2
+            if(curnum != "") ans.push(stoi(curnum));
+            ans.push(-2);
+            curnum = "";
+        }
+        else if(str[i] == 'U'){//-3
+            if(curnum != "") ans.push(stoi(curnum));
+            ans.push(-3);
+            curnum = "";
+        }
+        else if(str[i] == 'P'){//-4
+            if(curnum != "") ans.push(stoi(curnum));
+            ans.push(-4);
+            curnum = "";
+        }
+        else if(str[i] == 'C'){//-5
+            if(curnum != "") ans.push(stoi(curnum));
+            ans.push(-5);
+            curnum = "";
+        }
+    }
+    int fr, ccnt = 0;
+
+    while(!ans.empty()){
+        if(ans.front() == -1){//s
+            ans.pop();
+            if(ans.empty()) break;
+
+            fr -= ans.front();
+            ans.pop();
+        }
+        else if(ans.front() == -2){//m
+            ans.pop();
+            if(ans.empty()) break;
+
+            fr *= ans.front();
+            ans.pop();
+        }
+        else if(ans.front() == -3){//u
+            ans.pop();
+            if(ans.empty()) break;
+
+            fr /= ans.front();
+            ans.pop();
+        }
+        else if(ans.front() == -4){//p
+            ans.pop();
+            if(ans.empty()) break;
+
+            fr += ans.front();
+            ans.pop();
+        }
+        else if(ans.front() == -5){//c
+            cout << fr << ' ';
+            ccnt++;
+            ans.pop();
+        }
+        else{
+            fr = ans.front();
+            ans.pop();
+        }
+    }
+    if(ccnt == 0) cout << "NO OUTPUT";
 }
