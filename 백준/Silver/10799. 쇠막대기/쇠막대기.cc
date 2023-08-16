70fd02a5e0fa0d44ce232702c4c133b894d7bfdb
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-08-16 17:44:20 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+string s;
+int ans = 0;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> s;
+    stack<char> st;
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='(') st.push(s[i]);
+        else{ //')'
+            if(s[i-1]=='('){ //레이저인 경우
+                st.pop();
+                ans += st.size();
+            }
+            else{ //막대의 끝인 경우
+                st.pop();
+                ans++;
+            }
+        }
+    }
+    cout << ans;
+    return 0;
+}
