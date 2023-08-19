5facb46cab5a56f87022af8e3f114660581d1bf9
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-08-19 11:58:33 +0900

    [Silver IV] Title: Router, Time: 8 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/15828. Router/Router.cc
+++ 백준/Silver/15828. Router/Router.cc
@@ -2,13 +2,26 @@
 using namespace std;
 
 int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     int N, tmp;
     cin >> N;
     queue<int> q;
     while(cin >> tmp){
         if(tmp == -1) break;
-        else if(tmp == 0) q.pop();
-        else q.push(tmp);
+        else if(tmp == 0) {
+            q.pop();
+        }
+        else {
+            if(q.size() == N){
+                q.push(tmp);
+                q.pop();
+            }
+            else{
+                q.push(tmp);
+            }
+        }
     }
 
     if(q.empty()) cout << "empty";
