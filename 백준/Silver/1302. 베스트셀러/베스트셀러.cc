bf2a05f00317b04592714d04402e74a6cf579868
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-08-27 21:34:55 +0900

    [Silver IV] Title: 베스트셀러, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.cc
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.cc
@@ -1,26 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool cmp(pair<string, int> A, pair<string, int> B){
-    if(A.second == B.second){
-        return A.first < B.first;
-    }
-    return A.second > B.second;
-}
-
 int main(){
     map<string, int> m;
-    int N;
+    int N, cnt;
     cin >> N;
     string s;
     while(N--){
         cin >> s;
         m[s]++;
     }
-
-    vector<pair<string, int>> vec(m.begin(), m.end());
-
-    sort(vec.begin(), vec.end(), cmp);
-
-    cout << vec[0].first;
+    for(auto p : m) cnt = max(cnt, p.second);
+    for(auto p : m){
+        if(p.second == cnt){
+            cout << p.first;
+            return 0;
+        }
+    }
 }
