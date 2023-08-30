3642a4ad892ef56db4bcc2c461ce197401801641
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-08-30 11:58:12 +0900

    [Silver II] Title: 생태학, Time: 292 ms, Memory: 2292 KB -BaekjoonHub

---
+++ 백준/Silver/4358. 생태학/생태학.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    map<string, int> trees;
+    string str;
+
+    float cnt;
+
+    while(getline(cin, str)){
+        cnt++;
+        if(trees.find(str) != trees.end()){
+            trees[str]++;
+        }
+        else{
+            trees[str] = 1;
+        }
+    }
+
+    cout << fixed;
+    cout.precision(4);
+
+    for(auto it = trees.begin(); it != trees.end(); ++it)   
+        cout << it->first << " " << (it->second) * 100 / cnt << '\n';
+}
