4753903f54bd8ed0810612312418eaa6d0534df9
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-08-31 14:45:22 +0900

    [Gold V] Title: 무한 수열, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/1351. 무한 수열/무한 수열.cc
+++ 백준/Gold/1351. 무한 수열/무한 수열.cc
@@ -4,25 +4,8 @@ long long N, P, Q;
 map<long long, long long> m;
 long long DP(long long i){
     if(i == 0) return 1;
-    if(i == 1) return 2;
-    long long first, second;
-
-    if(m.find(floor(i / (float)P)) != m.end()){
-        first = m[floor(i / (float)P)];
-    }
-    else{
-        first = DP(floor(i / (float)P));
-        m[floor(i / (float)P)] = first;
-    }
-
-    if(m.find(floor(i / (float)Q)) != m.end()){
-        second = m[floor(i / (float)Q)];
-    }
-    else{
-        second = DP(floor(i / (float)Q));
-        m[floor(i / (float)Q)] = second;
-    }
-    return first + second;
+    if(m.find(i)!=m.end()) return m[i];
+    return m[i] = DP(i/P) + DP(i/Q);
 }
 int main(){
     ios_base::sync_with_stdio(0);
