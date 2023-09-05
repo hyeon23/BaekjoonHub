19e61e8eaa667b94439b0fc94d0835fc2119474a
hyeon <77566434+hyeon23@users.noreply.github.com>
2023-09-05 19:50:29 +0900

    [Gold V] Title: 가희와 프로세스 1, Time: 456 ms, Memory: 4336 KB -BaekjoonHub

---
+++ 백준/Gold/21773. 가희와 프로세스 1/가희와 프로세스 1.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct process{
+    int A;
+    int B;
+    int C;
+};
+
+struct cmp{
+    bool operator()(process p1, process p2){
+        if(p1.C == p2.C)
+            return p1.A > p2.A;
+        return p1.C < p2.C;
+    }
+};
+
+int main(){
+    //우선순위 큐: 우선순위 값 큰것 -> id가 가장 작은 것
+    //실행된 프로세스 -> 실행 필요 시간 - 1
+    //잔여 프로세스 -> 우선순위 + 1
+    int T, n, A, B, C;
+    cin >> T >> n;
+    priority_queue<process, vector<process>, cmp> pq;
+    for(int i = 0; i < n; ++i){
+        cin >> A >> B >> C;//pid, 프로세스 시간, 우선순위        
+        pq.push({A, B, C});
+    }
+
+    for(int i = 0; i < T; ++i){
+        if(pq.empty()) continue;
+
+        cout << pq.top().A << '\n';
+        if(pq.top().B-1 != 0){
+            process p = {pq.top().A, pq.top().B-1, pq.top().C-1};
+            pq.push(p);
+        }
+        pq.pop();
+    }
+    return 0;
+}
