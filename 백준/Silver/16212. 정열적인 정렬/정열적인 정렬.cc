62b1479711f884e890902a4501a58c664090f1ba
hyeon <77566434+hyeon23@users.noreply.github.com>
2023-09-10 23:57:12 +0900

    [Silver V] Title: 정열적인 정렬, Time: 144 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.cc
+++ 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.cc
@@ -2,16 +2,18 @@
 using namespace std;
 
 int main(){
-    int N;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int N, tmp;
     cin >> N;
-    vector<int> vec(N);
-    for(int i = 0; i < N; ++i){
+    vector<long long> vec(N);
+    
+    for(int i = 0; i < N; ++i)
         cin >> vec[i];
-    }
+
     sort(vec.begin(), vec.end());
 
-    for(int i = 0; i < N; ++i){
-        cout << vec[i] << ' ';
-    }
+    for(auto i : vec)
+        cout << i << ' ';
     return 0;
 }
