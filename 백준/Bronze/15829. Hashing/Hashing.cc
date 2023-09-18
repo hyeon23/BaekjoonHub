7e745712de11eba9b12365818d16bc6ac88ed7ee
hyeon <77566434+hyeon23@users.noreply.github.com>
2023-09-18 20:55:39 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -12,18 +12,18 @@ int main(){
     long long sum = 0;
 
     for(long long i = 0; i < N; ++i){
-        long long ai = (str[i] - 'a' + 1) % 1234567891;
+        long long ai = (str[i] - 'a' + 1);
 
         long long ri = 1;
 
         for(int j = 0; j < i; ++j){
-            ri *= 31 % 1234567891;
+            ri *= 31;
         }
         
         sum += ((ai % 1234567891) * (ri % 1234567891)) % 1234567891;
     }
 
-    cout << sum % 1234567891;
+    cout << sum;
 
     return 0;
 }
