c53374da438febe3dcd970d04924a5cfe7f2cd89
hyeon <77566434+hyeon23@users.noreply.github.com>
2023-09-18 20:51:28 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -3,27 +3,27 @@ using namespace std;
 
 int main(){
     //해시함수 -> 자료의 저장과 탐색에 쓰임
-    unsigned long long N;
+    long long N;
     cin >> N;
 
     string str;
     cin >> str;
 
-    unsigned long long sum = 0;
+    long long sum = 0;
 
-    for(unsigned long long i = 0; i < N; ++i){
-        unsigned long long ai = (str[i] - 'a' + 1) % 1234567891;
+    for(long long i = 0; i < N; ++i){
+        long long ai = (str[i] - 'a' + 1) % 1234567891;
 
-        unsigned long long ri = 1;
+        long long ri = 1;
 
         for(int j = 0; j < i; ++j){
             ri *= 31 % 1234567891;
         }
         
-        sum += ai * ri % 1234567891;
+        sum += ((ai % 1234567891) * (ri % 1234567891)) % 1234567891;
     }
 
-    cout << sum;
+    cout << sum % 1234567891;
 
     return 0;
 }
