3154d7a3100039e321fa822161076db1982595ff
hyeon <77566434+hyeon23@users.noreply.github.com>
2023-09-19 14:05:57 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 3, Time: 92 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M, R, u, v;
+vector<bool> visited(100001);
+int depth = 0;
+
+void dfs(vector<vector<int>>& vec, vector<int>& ans, int point){
+    //방문 체크
+    if(visited[point]) return;
+    
+    //현재 점 방문
+    visited[point] = true;
+
+    ans[point] = depth;
+
+    depth++;
+
+    sort(vec[point].begin(), vec[point].end());
+
+    for(int i = 0; i < vec[point].size(); ++i){
+        dfs(vec, ans, vec[point][i]);
+    }
+
+    depth--;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    cin >> N >> M >> R;
+
+    vector<vector<int>> vec(N+1);
+    vector<int> ans(N+1, -1);
+
+    for(int i = 0; i < M; ++i){
+        cin >> u >> v;
+        vec[u].push_back(v);
+        vec[v].push_back(u);
+    }
+
+    dfs(vec, ans, R);
+
+    for(int i = 1; i <= N; ++i){
+        cout << ans[i] << '\n';
+    }
+
+    return 0;
+}
