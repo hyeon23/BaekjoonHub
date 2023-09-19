7c719fea51d2d2c5f0aac895d45540ea632483d5
hyeon <77566434+hyeon23@users.noreply.github.com>
2023-09-19 23:50:45 +0900

    [Silver II] Title: 나무 자르기, Time: 16 ms, Memory: 3012 KB -BaekjoonHub

--- 백준/Silver/14247. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/14247. 나무 자르기/나무 자르기.cc
@@ -1,36 +1,22 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define ll long long
 using namespace std;
-
-bool cmp(pair<long long, long long> A, pair<long long, long long> B){
-    if(A.second == B.second){
-        A.first > B.first;
-    }
-    return A.second < B.second;
-}
-
 int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    long long n;
-    cin >> n;
-    vector<pair<long long, long long>> trees(n);//높이, 성장률
+	cin.tie(0);
+	ios_base::sync_with_stdio(0);
+	int n;
+	cin>>n;
+    vector<pair<int,ll>> tree(n);
 
-    for(long long i = 0; i < n; ++i)
-        cin >> trees[i].first;
-    for(long long i = 0; i < n; ++i)
-        cin >> trees[i].second;
+	for(int i=0;i<n;++i) cin>>tree[i].second;
+	for(int i=0;i<n;++i) cin>>tree[i].first;
+	sort(tree.begin(),tree.end());
 
-    sort(trees.begin(), trees.end(), cmp);
-
-    long long sum = 0;
-
-    for(long long i = 0; i < n; ++i){
-        sum += trees[i].first;
-        for(long long j = i+1; j < n; ++j){
-            trees[j].first += trees[j].second;
-        }
-    }
-
-    cout << sum;
-    return 0;
+	ll yang=0;
+	for(int i=0;i<n;++i){
+		yang += tree[i].second + tree[i].first * i;
+	}
+	cout<<yang;
 }
