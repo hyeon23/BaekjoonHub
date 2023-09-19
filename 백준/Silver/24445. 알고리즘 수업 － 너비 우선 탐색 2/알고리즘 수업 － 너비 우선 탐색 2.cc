1bb592aebd950b3f98e442976311f710de3c9644
hyeon <77566434+hyeon23@users.noreply.github.com>
2023-09-19 19:10:12 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 216 ms, Memory: 8708 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M, R, u, v;
+vector<vector<int>> vec(100001);
+vector<bool> visited(100001);
+vector<int> ans(100001, 0);
+
+int rnk = 1;
+
+void bfs(int cur){
+    visited[cur] = true;
+
+    queue<int> bfsQ;
+
+    bfsQ.push(cur);
+
+    while(!bfsQ.empty()){
+
+        int f = bfsQ.front();
+
+        bfsQ.pop();
+
+        ans[f] = rnk;
+
+        rnk++;
+
+        for(int i = 0; i < vec[f].size(); ++i){
+            if(!visited[vec[f][i]]){
+                visited[vec[f][i]] = true;
+                bfsQ.push(vec[f][i]);
+            }
+        }
+    }
+}
+
+int main(){    
+    cin >> N >> M >> R;
+
+    for(int i = 1; i <= M; ++i){
+        cin >> u >> v;
+        vec[u].push_back(v);
+        vec[v].push_back(u);
+    }
+
+    for(int i = 1; i <= N; ++i){
+        sort(vec[i].begin(), vec[i].end(), greater<int>());
+    }
+
+    bfs(R);//depth 0부터 탐색
+
+    for(int i = 1; i <= N; ++i){
+        cout << ans[i] << '\n';
+    }
+}
