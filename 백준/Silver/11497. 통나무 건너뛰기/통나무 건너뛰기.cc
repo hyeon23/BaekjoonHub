058340488a6abad037582967c7b7d7256318118b
hyeon <77566434+hyeon23@users.noreply.github.com>
2023-09-20 20:19:42 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 124 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.cc
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.cc
@@ -1,31 +1,28 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;	
 
-int main(){
-    int T;
-    cin >> T;
-    while(T--){
-        int N, tmp;
-        cin >> N;
-        vector<int> vec(N);
-        for(int i = 0; i < N; ++i) cin >> vec[i];
-        sort(vec.begin(), vec.end(), greater<int>());
-        deque<int> dq;
+int main() {
+	int T;
+	cin >> T;
 
-        for(int i = 0; i < N; ++i){
-            if(i % 2 == 0){
-                dq.push_front(vec[i]);
-            }
-            else{
-                dq.push_back(vec[i]);
-            }
-        }
-        dq.push_back(dq[0]);
-        int dif = 0;
-        for(int i = 1; i <= N; ++i){
-            dif = max(dif, abs(dq[i] - dq[i-1]));
-        }
-        cout << dif << '\n';
-    } 
-    return 0;
+	while (T--) {
+		int N;
+		cin >> N;
+
+		vector <int> v(N);
+
+		for (int i = 0; i < N; i++) cin >> v[i];
+
+		sort(v.begin(), v.end());
+
+		int result = 0;
+		result = max(v[1] - v[0], v[N - 1] - v[N - 2]);
+
+		for (int i = 0; i < N - 2; i++) {
+			result = max(result, v[i + 2] - v[i]);
+		}
+		cout << result << '\n';
+	}
 }
