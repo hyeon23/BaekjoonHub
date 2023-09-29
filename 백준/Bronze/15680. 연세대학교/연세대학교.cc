4051e441173151261204833220048840b5cfc506
hyeon <77566434+hyeon23@users.noreply.github.com>
2023-09-29 15:17:07 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int i;
+    cin >> i;
+
+    if(i == 0) cout << "YONSEI";
+    else if(i == 1) cout << "Leading the Way to the Future";
+}
