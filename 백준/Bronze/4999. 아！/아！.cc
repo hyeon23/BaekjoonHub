35a716aabbd128383e14bd85e0372dbd2b0328d4
hyeon <77566434+hyeon23@users.noreply.github.com>
2023-09-29 15:08:45 +0900

    [Bronze V] Title: 아!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string jae, doc;
+    cin >> jae >> doc;
+
+    jae.size() < doc.size() ? cout << "no" : cout << "go";
+
+    return 0;
+}
