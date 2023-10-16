bfb43c6f0096e84bda85db2cba4d1fc73a07643f
hyeon <77566434+hyeon23@users.noreply.github.com>
2023-10-16 21:58:23 +0900

    [Silver II] Title: 양 한마리... 양 두마리..., Time: 28 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.cc
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int H, W;
+
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+
+void dfs(pair<int, int> cur, vector<vector<char>>& vvec, vector<vector<bool>>& visited){
+    stack<pair<int, int>> stk;
+
+    visited[cur.first][cur.second] = true;
+    stk.push(cur);
+
+    while(!stk.empty()){
+        pair<int, int> curpos = stk.top();
+        stk.pop();
+
+        for(int i = 0; i < 4; ++i){
+            int nx = curpos.first + dx[i];
+            int ny = curpos.second + dy[i];
+
+            if(nx < 0 || ny < 0 || nx >= H || ny >= W) continue;
+
+            if(visited[nx][ny] || vvec[nx][ny] == '.') continue;
+
+            visited[nx][ny] = true;
+            stk.push({nx, ny});
+        }
+    }
+}
+
+int main(){
+    //# 양, .풀
+
+    int T;
+    cin >> T;
+
+    while(T--){
+        int cnt = 0;
+        
+        cin >> H >> W;
+        
+        vector<vector<char>> vvec(101, vector<char>(101));
+        vector<vector<bool>> visited(101, vector<bool>(101, false));
+
+        for(int i = 0; i < H; ++i){
+            for(int j = 0; j < W; ++j){
+                cin >> vvec[i][j];
+            }
+        }
+
+        for(int i = 0; i < H; ++i){
+            for(int j = 0; j < W; ++j){
+                if(!visited[i][j] && vvec[i][j] == '#'){
+                    dfs({i, j}, vvec, visited);
+                    ++cnt;
+                }
+            }
+        }
+
+        cout << cnt << '\n';
+    }
+    return 0;
+}
