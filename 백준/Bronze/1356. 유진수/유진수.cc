a046d0994cc41b8c235ab18bf2c9f8355e7d3f2e
hyeon <77566434+hyeon23@users.noreply.github.com>
2023-10-25 13:41:07 +0900

    [Bronze I] Title: 유진수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1356. 유진수/유진수.cc
+++ 백준/Bronze/1356. 유진수/유진수.cc
@@ -9,19 +9,16 @@ int main() {
     for (int i = 0; i < k.size() - 1; i++)//나누는 횟수
     {
         pivot++;
-        l = 1;
-        r = 1;
-        ans = 0;
-        for (int j = 0; j < pivot; j++)
-            l *= k[j] - '0';
-        for (int t = pivot; t < k.size(); t++)
-            r *= k[t] - '0';
+        l = 1; r = 1; ans = 0;
+        for (int j = 0; j < pivot; j++) l *= k[j] - '0';
+        for (int t = pivot; t < k.size(); t++) r *= k[t] - '0';
+        
         if (l == r)
         {
             ans = 1;
             break;
         }
     }
-    if (ans == 1) { cout << "YES" << '\n'; }
-    else { cout << "NO" << '\n'; }
+    if (ans == 1) cout << "YES" << '\n';
+    else cout << "NO" << '\n';
 }
