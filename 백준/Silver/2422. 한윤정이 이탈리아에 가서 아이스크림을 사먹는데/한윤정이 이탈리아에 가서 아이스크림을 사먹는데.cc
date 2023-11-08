068088f1a713253e7acbac18b911fc3295a3bad1
hyeon <77566434+hyeon23@users.noreply.github.com>
2023-11-08 22:39:15 +0900

    [Silver IV] Title: 한윤정이 이탈리아에 가서 아이스크림을 사먹는데, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.cc
+++ 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.cc
@@ -1,38 +1,29 @@
 #include <iostream>
-#include <vector>
 using namespace std;
+
+int N, M;
+bool check[205][205];
 
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    
-    int N, M, ans = 0;
+
     cin >> N >> M;
-
-    vector<vector<bool>> isForbidden(N, vector<bool>(N, false));
-
     for(int i = 0; i < M; ++i){
-        int x, y;
-        cin >> x >> y;
-
-        // Mark pairs of ice creams as forbidden
-        isForbidden[x - 1][y - 1] = true;
-        isForbidden[y - 1][x - 1] = true;
+        int p, q;
+        cin >> p >> q;
+        check[p][q] = check[q][p] = true;
     }
+    int ans = 0;
+    for(int i = 1; i <= N; ++i){
+        for(int j = i+1; j <= N; ++j){
+            if(check[i][j]) continue;
 
-    // Try all combinations of 3 ice creams
-    for(int i = 0; i < N; ++i){
-        for(int j = i + 1; j < N; ++j){
-            for(int k = j + 1; k < N; ++k){
-                // Check if the combination is not forbidden
-                if (!isForbidden[i][j] && !isForbidden[i][k] && !isForbidden[j][k]) {
-                    ++ans;
-                }
+            for(int k = j+1; k <= N; ++k){
+                if(check[i][k] || check[j][k]) continue;
+                ++ans;
             }
         }
     }
-
-    cout << ans;
-
-    return 0;
+    cout << ans << '\n';
 }
