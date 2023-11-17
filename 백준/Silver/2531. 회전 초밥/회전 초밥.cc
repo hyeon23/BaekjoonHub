ada9b27f5d0b9ee388b3ff07a0d31139e41c2510
hyeon <77566434+hyeon23@users.noreply.github.com>
2023-11-17 22:55:26 +0900

    [Silver I] Title: 회전 초밥, Time: 664 ms, Memory: 2136 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.cc
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.cc
@@ -1,12 +1,9 @@
-#include <iostream>
-#include <algorithm>
-#include <cstring>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int n, d, k, c; // 접시의 수, 초밥의 가짓수, 연속해서 먹는 접시의 수, 쿠폰 번호
 int arr[30001]; // 회전 초밥 테이블
-bool check[30001];
 int maxCnt = 0;
 
 int main() {
@@ -16,6 +13,7 @@ int main() {
     }
 
     for(int i = 0; i < n; i++) {
+        vector<bool> check(30001, 0);
         int flag = 0;
         int coupon = 1;
         for(int j = i; j < i + k; j++) {
@@ -24,7 +22,6 @@ int main() {
         }
         if(check[c]) coupon = 0;
         maxCnt = max(maxCnt, k - flag + coupon);
-        memset(check, false, sizeof(check));
     }
     cout << maxCnt << endl;
     return 0;
