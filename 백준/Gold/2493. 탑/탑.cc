d8ca49d4bc544c3172b2ee6836acb20e512bdabd
Hyeon <77566434+hyeon23@users.noreply.github.com>
2023-12-08 15:49:52 +0900

    [Gold V] Title: 탑, Time: 104 ms, Memory: 6300 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -1,39 +1,18 @@
-#include <iostream>
-#include <vector>
-#include <deque>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct st {
-    int x;
-    int y;
-};
-
-int main() {
-    int N, tmp;
-    deque<st> stk;
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int N, height;
     cin >> N;
-
-    vector<int> vec(N + 1, 0);
-
-    for (int i = 1; i <= N; ++i) {
-        cin >> tmp;
-        st tower = {i, tmp};
-        
-        // 현재 탑보다 작은 높이의 탑들을 스택에서 제거
-        while (!stk.empty() && stk.back().y < tmp) {
-            stk.pop_back();
-        }
-        
-        if (!stk.empty()) {
-            vec[i] = stk.back().x; // 남은 탑 중에서 신호를 받을 수 있는 경우
-        }
-
-        stk.push_back(tower);
+    stack<pair<int, int>> stk;
+    stk.push({100000001, 0});
+    for(int i = 1; i <= N; ++i){
+        cin >> height;
+        while(!stk.empty() && stk.top().first < height) stk.pop();
+        cout << stk.top().second << ' ';
+        stk.push({height, i});
     }
-
-    for (int i = 1; i <= N; ++i) {
-        cout << vec[i] << ' ';
-    }
-
     return 0;
 }
