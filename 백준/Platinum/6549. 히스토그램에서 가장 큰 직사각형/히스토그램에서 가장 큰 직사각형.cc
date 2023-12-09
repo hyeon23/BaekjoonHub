5b631c570b2e0e1ad14b1962ed07acc71ed47425
Hyeon <77566434+hyeon23@users.noreply.github.com>
2023-12-10 03:21:57 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 36 ms, Memory: 2280 KB -BaekjoonHub

--- 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.cc
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.cc
@@ -2,37 +2,30 @@
 using namespace std;
 #define X first
 #define Y second
-
-int main(void)
-{
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0); cout.tie(0);
+  while (true) {
     int n;
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    while (true)
-    {
-        cin >> n;
-        if (n == 0) break;
-        stack<pair<long long, long long>> S;
-        long long ans = 0;
-        for (int i = 0; i < n; i++)
-        {
-            int h;
-            cin >> h;
-            int idx = i;
-            while (!S.empty() && S.top().X >= h)
-            {
-                ans = max(ans, (i - S.top().Y) * S.top().X);
-                idx = S.top().Y;
-                S.pop();
-            }
-            S.push({h, idx});
-        }
-        while (!S.empty())
-        {
-            ans = max(ans, (n - S.top().Y) * S.top().X);
-            S.pop();
-        }
-        cout << ans << '\n';
+    cin >> n;
+    if (n == 0) break;
+    stack<pair<int, int>> S;
+    long long ans = 0;
+    for (int i = 0; i < n; i++) {
+      int h;
+      cin >> h;
+      int idx = i;
+      while (!S.empty() && S.top().X >= h) {//스택이 비거나 h보다 더 낮은 top이 나올때까지.
+        ans = max(ans, 1LL * (i - S.top().Y) * S.top().X);
+        idx = S.top().Y;
+        S.pop();
+      }
+      S.push({h, idx}); //top보다 더 높은 h를 만나면 쌓아둔다.
     }
+    while (!S.empty()) {
+      ans = max(ans, 1LL * (n - S.top().Y) * S.top().X);
+      S.pop();
+    }
+    cout << ans << '\n';
+  }
 }
