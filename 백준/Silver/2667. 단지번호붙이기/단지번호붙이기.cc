db1ffb5aeaa9444cb6150cf95c1301b4d460cdfb
Hyeon <77566434+hyeon23@users.noreply.github.com>
2023-12-14 15:57:56 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+char board[26][26] = {0,};
+bool visited[26][26] = {false,};
+vector<int> ans;
+void bfs(pair<int, int> cur){
+    int answer = 0;
+    queue<pair<int, int>> que;
+    que.push(cur);
+    while(!que.empty()){
+        int cx = que.front().first;
+        int cy = que.front().second;
+        que.pop();
+        ++answer;
+        for(int i = 0; i < 4; ++i){
+            int nx = cx + dx[i];
+            int ny = cy + dy[i];
+            if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+            if(visited[nx][ny] || board[nx][ny] == '0') continue;
+            visited[nx][ny] = true;
+            que.push({nx, ny});
+        }
+    }
+    ans.push_back(answer);
+}
+int main(){
+    cin >> N;
+    for(int i = 0; i < N; ++i){
+        for(int j = 0; j < N; ++j){
+            cin >> board[i][j];
+        }
+    }
+    for(int i = 0; i < N; ++i){
+        for(int j = 0; j < N; ++j){
+            if(!visited[i][j] && board[i][j] == '1') {
+                visited[i][j] = true;
+                bfs({i, j});
+            }
+        }
+    }
+    sort(ans.begin(), ans.end());
+    cout << ans.size() << '\n';
+    for(auto i : ans){
+        cout << i << '\n';
+    }
+    return 0;
+}
