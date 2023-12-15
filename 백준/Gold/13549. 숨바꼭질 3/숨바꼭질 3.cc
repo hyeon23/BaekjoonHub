aa7b5518bd9819e72fecb1f913ebddc453a8041f
Hyeon <77566434+hyeon23@users.noreply.github.com>
2023-12-16 05:57:45 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -1,34 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int n,k;
-int dist[100001];
-int main(void) {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-
-  cin >> n >> k;
-  int MX = 100001;
-  fill(dist,dist+MX,-1);
-  deque<int> dq;
-  dq.push_back(n);
-  dist[n]=0;
-  while(!dq.empty()){
-    int cur = dq.front(); 
-    dq.pop_front();
-    if( 2*cur < MX && dist[2*cur] == -1){
-      dist[2*cur] = dist[cur];
-      dq.push_front(2*cur);
+int n, k, ans = INT_MAX;
+vector<bool> visited(100001, false);
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    cin >> n >> k;
+    deque<pair<int, int>> dq;
+    visited[n] = true;
+    dq.push_back({n, 0});
+    while (!dq.empty())
+    {
+        int cur = dq.front().first;
+        int ccnt = dq.front().second;
+        dq.pop_front();
+        if(cur == k){
+            ans = min(ans, ccnt);
+        }
+        if (2 * cur < 100001 && !visited[2 * cur])
+        {
+            visited[2 * cur] = true;
+            dq.push_front({2 * cur, ccnt});
+        }
+        for (int nxt : {cur - 1, cur + 1})
+        {
+            if (nxt < 0 or nxt >= 100001 or visited[nxt]) continue;
+            visited[nxt] = true;
+            dq.push_back({nxt, ccnt + 1});
+        }
     }
-    for(int nxt : {cur-1,cur+1}){
-      if(nxt < 0 or nxt >= MX or dist[nxt] != -1) continue;
-      dist[nxt] = dist[cur]+1;
-      dq.push_back(nxt);      
-    }
-  } 
-  cout << dist[k];
+    cout << ans;
 }
-
-/*
-0-1 BFS로 해결한 풀이
-*/
