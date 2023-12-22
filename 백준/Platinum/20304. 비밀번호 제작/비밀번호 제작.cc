10372594c4d8cf0f313b85a2be170d9b674688b3
Hyeon <77566434+hyeon23@users.noreply.github.com>
2023-12-22 20:55:19 +0900

    [Platinum V] Title: 비밀번호 제작, Time: 100 ms, Memory: 12936 KB -BaekjoonHub

---
+++ 백준/Platinum/20304. 비밀번호 제작/비밀번호 제작.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N, M;
+int max_bit_size = 0; // 최대 비트 크기를 저장하는 변수
+vector<int> passwards; // 비밀번호 목록을 저장하는 배열
+vector<int> visited(1000001, 0); // 방문 여부를 저장하는 배열
+vector<int> depth(1000001, 0); // 시작점으로부터의 깊이를 저장하는 배열
+void Make_max_bit_size(){
+    int check = N;
+    // N을 2로 나누면서 최대 비트 크기 계산
+    while(check){
+        check /= 2;
+        max_bit_size++;
+    }
+}
+int Bfs(){
+    queue<int> que;
+    // 초기 비밀번호들을 시작점으로 설정하여 BFS 수행
+    for(int i = 0; i < passwards.size(); ++i){
+        que.push(passwards[i]);
+        visited[passwards[i]] = 1;
+        depth[passwards[i]] = 0;
+    }
+    while(!que.empty()){
+        int from = que.front();
+        que.pop();
+        // 현재 비밀번호의 각 비트를 토글하여 새로운 비밀번호 생성
+        for(int i = 0; i < max_bit_size; ++i){
+            int to = (from ^ (1 << i)); // 비트 토글 연산 (XOR)
+            // 새로운 비밀번호가 범위 내에 있고 아직 방문하지 않았다면 큐에 추가
+            if(to <= N && visited[to] == 0){
+                visited[to] = 1;
+                depth[to] = depth[from] + 1;
+                que.push(to);
+            }
+        }
+    }
+    int ret = -1;
+    // 최종적으로 방문한 비밀번호들 중에서 가장 깊은 깊이를 찾아 반환
+    for(int i = 0; i <= N; ++i) ret = max(ret, depth[i]);
+    return ret;
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin >> N >> M;
+    int passward;
+    for(int i = 0; i < M; ++i){
+        cin >> passward;
+        passwards.push_back(passward);
+    }
+
+    Make_max_bit_size(); // 최대 비트 크기 계산
+    cout << Bfs(); // BFS 수행 결과 출력
+}
