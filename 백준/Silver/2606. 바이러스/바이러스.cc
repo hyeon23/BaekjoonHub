571d67ad8701b60211c2d537d1d9e54f905559c8
Hyeon <77566434+hyeon23@users.noreply.github.com>
2023-12-22 21:51:37 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -1,34 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 vector<vector<int>> vvec(101);
-
 bool visited[101];
-
 int ans = 0;
-
 void dfs(int level){
     stack<int> stk;
-
     visited[level] = true;
-
     stk.push(level);
-
     while(!stk.empty()){
         int cur = stk.top();
         stk.pop();
-
         for(int i = 0; i < vvec[cur].size(); ++i){
             if(visited[vvec[cur][i]]) continue;
-
             ans++;
-
             visited[vvec[cur][i]] = true;
             stk.push(vvec[cur][i]);
         }
     }
 }
-
 int main(){
     int N, M, x, y;
     cin >> N >> M;
@@ -37,10 +26,7 @@ int main(){
         vvec[x].push_back(y);
         vvec[y].push_back(x);
     }
-
     dfs(1);
-
     cout << ans;
-
     return 0;
 }
