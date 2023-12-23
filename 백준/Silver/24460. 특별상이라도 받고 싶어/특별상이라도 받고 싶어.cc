077bf549a917f4b9a16649a6cc21084b2ca3f211
Hyeon <77566434+hyeon23@users.noreply.github.com>
2023-12-23 23:49:41 +0900

    [Silver III] Title: 특별상이라도 받고 싶어, Time: 184 ms, Memory: 6256 KB -BaekjoonHub

--- 백준/Silver/24460. 특별상이라도 받고 싶어/특별상이라도 받고 싶어.cc
+++ 백준/Silver/24460. 특별상이라도 받고 싶어/특별상이라도 받고 싶어.cc
@@ -20,6 +20,8 @@ int recursive(int x, int y, int n){
     return ans[1];
 }
 int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
     cin >> N;
     for(int i = 0; i < N; ++i){
         for(int j = 0; j < N; ++j){
