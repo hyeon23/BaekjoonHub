461d98660ef73be1c143c6897c3a7e7078026fbb
Hyeon <77566434+hyeon23@users.noreply.github.com>
2023-12-25 23:43:38 +0900

    [Silver II] Title: 투에-모스 문자열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/18222. 투에－모스 문자열/투에－모스 문자열.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    unsigned long long n;
+    cin >> n;
+    cout << bitset<64>(--n).count() % 2;
+    return 0;
+}
