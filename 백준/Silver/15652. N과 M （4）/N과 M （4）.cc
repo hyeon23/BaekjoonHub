6aba0c0b9b7a2fa28d3400ab6c86ca2ed8bc9478
Hyeon <77566434+hyeon23@users.noreply.github.com>
2024-01-10 02:56:04 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -1,28 +1,22 @@
-#include <iostream>
-#define MAX 9
+#include <bits/stdc++.h>
 using namespace std;
-
-int n, m;
-int arr[MAX];
-
-void DFS(int depth, int num)
-{
-    if (depth == m)
-    {
-        for (int i = 0; i < m; ++i)
-            cout << arr[i] << ' ';
+int N, M;
+vector<int> vec(9);
+void DFS(int depth, int num){
+    if(depth == M){
+        for(int i = 0; i < M; ++i){
+            cout << vec[i] << ' ';
+        }
         cout << '\n';
         return;
     }
-
-    for(int i = num; i <= n; ++i){
-        arr[depth] = i;
-        DFS(depth + 1, i);
+    for(int i = num; i <= N; ++i){
+        vec[depth] = i;
+        DFS(depth+1, i);
     }
 }
-
 int main(){
-    cin >> n >> m;
+    cin >> N >> M;
     DFS(0, 1);
     return 0;
 }
