8b9d76277be5b6e48425c0201b9b6de914c8c3ca
Hyeon <77566434+hyeon23@users.noreply.github.com>
2024-01-10 00:16:38 +0900

    [Silver III] Title: N과 M (1), Time: 32 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,30 +1,26 @@
-#include <iostream>
-#define MAX 9
+#include <bits/stdc++.h>
 using namespace std;
-
-int n, m;
-int arr[MAX];//정렬된 순열을 담는 배열
-bool visited[MAX];//이미 방문된 숫자인지 판별하는 bool 배열
-
-void DFS(int depth){
-    if(depth == m){
-        for(int i = 0; i < m; ++i){
-            cout << arr[i] << " ";
+int N, M;
+vector<int> arr(9, 0);
+vector<bool> visited(9, false);
+void DFS(int n){
+    if(n == M){
+        for(int i = 0; i < M; ++i){
+            cout << arr[i] << ' ';
         }
         cout << '\n';
         return;
     }
-    for(int i = 1; i <= n; ++i){
+    for(int i = 1; i <= N; ++i){
         if(!visited[i]){
             visited[i] = true;
-            arr[depth] = i;
-            DFS(depth + 1);
+            arr[n] = i;
+            DFS(n+1);
             visited[i] = false;
         }
     }
 }
-
-int main() {
-  cin >> n >> m;
-  DFS(0);
+int main(){
+    cin >> N >> M;
+    DFS(0);
 }
