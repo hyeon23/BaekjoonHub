82b7e3de2f25e33c8632571cf8335d5c7e589e49
Hyeon <77566434+hyeon23@users.noreply.github.com>
2024-01-10 17:09:31 +0900

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -4,21 +4,27 @@ int N, M;
 vector<int> input;
 vector<int> vec(9);
 vector<bool> visited(9);
-set<vector<int>> ans;
+// set<vector<int>> ans;
 //중복되는 수열 출력 불가
 //사전 순으로 증가
 void DFS(int depth){
     if(depth == M){
-        vector<int> anstmp;
-        for(int i = 0; i < M; ++i)
-            anstmp.push_back(vec[i]);
-        ans.insert(anstmp);
+        for (int i = 0; i < M; ++i) {
+          cout << vec[i] << ' ';
+        }
+        cout << '\n';
+        // vector<int> anstmp;
+        // for(int i = 0; i < M; ++i)
+        //     anstmp.push_back(vec[i]);
+        // ans.insert(anstmp);
         return;
     }
+    int tmp = 0;
     for(int i = 0; i < N; ++i){
-        if(!visited[i]){
+        if(!visited[i] && tmp != input[i]){
             visited[i] = true;
             vec[depth] = input[i];
+            tmp = vec[depth];
             DFS(depth+1);
             visited[i] = false;
         }
@@ -33,11 +39,11 @@ int main(){
     }
     sort(input.begin(), input.end());
     DFS(0);
-    for(auto s : ans){
-        for(auto i : s){
-            cout << i << ' ';
-        }
-        cout << '\n';
-    }
+    // for(auto s : ans){
+    //     for(auto i : s){
+    //         cout << i << ' ';
+    //     }
+    //     cout << '\n';
+    // }
     return 0;
 }
