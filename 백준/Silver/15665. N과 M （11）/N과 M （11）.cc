72d46bfe7747529fe65de1fc91dff1f82f99e97e
Hyeon <77566434+hyeon23@users.noreply.github.com>
2024-01-10 18:50:25 +0900

    [Silver II] Title: N과 M (11), Time: 520 ms, Memory: 32652 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N, M;
+vector<int> input;
+vector<int> vec(9);
+set<vector<int>> ans;
+//중복되는 수열 출력 불가
+//사전 순으로 증가
+void DFS(int depth){
+    if(depth == M){
+        vector<int> anstmp;
+        for(int i = 0; i < M; ++i)
+            anstmp.push_back(vec[i]);
+        ans.insert(anstmp);
+        return;
+    }
+    for(int i = 0; i < N; ++i){
+        vec[depth] = input[i];
+        DFS(depth+1);
+    }
+}
+int main(){
+    cin >> N >> M;
+    for(int i = 0; i < N; ++i){
+        int tmp;
+        cin >> tmp;
+        input.push_back(tmp);
+    }
+    sort(input.begin(), input.end());
+    DFS(0);
+    for(auto s : ans){
+        for(auto i : s){
+            cout << i << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
