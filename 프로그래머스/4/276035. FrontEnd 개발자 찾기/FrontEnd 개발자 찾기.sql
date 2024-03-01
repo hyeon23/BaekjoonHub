bffcf8e47bffc863b8fb1912b667dde189bbb3bc
Hyeon <77566434+hyeon23@users.noreply.github.com>
2024-03-01 10:56:37 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,18 +1,8 @@
-SELECT
-    ID
-    , EMAIL
-    , FIRST_NAME
-    , LAST_NAME
-FROM
-    DEVELOPERS 
-WHERE
-    SKILL_CODE & (
-        SELECT
-            SUM(CODE)
-        FROM
-            SKILLCODES
-        WHERE
-            CATEGORY = 'Front End'
-    )
-ORDER BY
-    ID
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM DEVELOPERS 
+WHERE SKILL_CODE & (
+SELECT SUM(CODE)
+FROM SKILLCODES
+WHERE CATEGORY = 'Front End'
+)
+ORDER BY ID
