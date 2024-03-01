a3e2cf3efb29975d5534ca7862a90e3a2a565ad0
Hyeon <77566434+hyeon23@users.noreply.github.com>
2024-03-01 21:28:51 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 12 ms, Memory: 2944 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
@@ -1,65 +1,3 @@
-// #include <bits/stdc++.h>
-// using namespace std;
-// int N, K, ans = INT_MAX;
-// vector<int> ansvec;
-// bool visited[100001] = {false,};
-
-// struct DATA{
-//     int x;
-//     int cnt;
-//     vector<int> root;
-// };
-// void bfs(int n){
-//     queue<DATA> que;
-//     vector<int> vec;
-//     vec.push_back(n);
-//     visited[n] = true;
-//     que.push({n, 0, vec});
-//     while(!que.empty()){
-//         DATA cur = que.front();
-//         if(cur.x == K){
-//             ansvec = cur.root;
-//             ans = cur.cnt;
-//             break;
-//         }
-//         que.pop();
-//         int nx;
-//         int nc;
-//         vector<int> tmp1 = cur.root;
-//         nx = cur.x + 1;
-//         nc = cur.cnt;
-//         if(!(nx < 0 || nx >= 100001 || visited[nx])) {
-//             visited[nx] = true;
-//             tmp1.push_back(nx);
-//             que.push({nx, nc+1, tmp1});
-//         }
-//         vector<int> tmp2 = cur.root;
-//         nx = cur.x - 1;
-//         if(!(nx < 0 || nx >= 100001 || visited[nx])) {
-//             visited[nx] = true;
-//             tmp2.push_back(nx);
-//             que.push({nx, nc+1, tmp2});
-//         }
-//         vector<int> tmp3 = cur.root;
-//         nx = cur.x * 2;
-//         if(!(nx < 0 || nx >= 100001 || visited[nx])) {
-//             visited[nx] = true;
-//             tmp3.push_back(nx);
-//             que.push({nx, nc+1, tmp3});
-//         }
-//     }
-// }
-// int main(){
-//     ios::sync_with_stdio(0);
-//     cin.tie(0); cout.tie(0);
-//     cin >> N >> K;
-//     //걷는다 or 순간이동한다.
-//     bfs(N);
-//     cout << ans << '\n';
-//     for(auto i : ansvec) cout << i << ' ';
-//     return 0;
-// }
-
 #include <bits/stdc++.h>
 using namespace std;
 int N, K, ans = INT_MAX, ansidx = 0;
@@ -83,20 +21,20 @@ void bfs(int n){
         int nx;
         int nc;
         nx = cur.x + 1;
-        nc = cur.cnt;
+        nc = cur.cnt + 1;
         if(!(nx < 0 || nx >= 100001 || visited[nx] != -1)) {
             visited[nx] = cur.x;
-            que.push({nx, nc+1});
+            que.push({nx, nc});
         }
         nx = cur.x - 1;
         if(!(nx < 0 || nx >= 100001 || visited[nx] != -1)) {
             visited[nx] = cur.x;
-            que.push({nx, nc+1});
+            que.push({nx, nc});
         }
         nx = cur.x * 2;
         if(!(nx < 0 || nx >= 100001 || visited[nx] != -1)) {
             visited[nx] = cur.x;
-            que.push({nx, nc+1});
+            que.push({nx, nc});
         }
     }
 }
